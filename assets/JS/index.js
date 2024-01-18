dd358937690e10583c109ccb7ae842d571907ef4
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-18 18:17:08 -0400

    feat: melhoria na renderização das flores

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -19,3 +19,60 @@ document.querySelector('.close').addEventListener('click', () => {
 document.querySelector('.menu-lateral-over').addEventListener('click', () => {
     document.querySelector('.menu-lateral').classList.remove('active')
 })
+
+const flores = [
+    {
+        id: 1,
+        url: 'assets/img/image-1.png',
+        price: 85,
+    },
+    {
+        id: 2,
+        url: 'assets/img/image-2.png',
+        price: 30,
+    },
+    {
+        id: 3,
+        url: 'assets/img/image-3.png',
+        price: 50,
+    },
+    {
+        id: 4,
+        url: 'assets/img/image-4.png',
+        price: 10,
+    },
+    {
+        id: 5,
+        url: 'assets/img/image-5.png',
+        price: 50,
+    },
+    {
+        id: 6,
+        url: 'assets/img/image-6.png',
+        price: 20,
+    },
+]
+
+function convertToReal(value) {
+    return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+}
+
+flores.forEach((item) => {
+    const ele = document.createElement('div')
+    ele.classList.add('container')
+
+    const img = document.createElement('img')
+    img.src = item.url
+    img.alt = 'Flor'
+
+    const price = document.createElement('div')
+    price.classList.add('desc')
+    price.textContent = convertToReal(item.price)
+    price.addEventListener('click', () => {
+        compra()
+    })
+
+    ele.appendChild(img)
+    ele.appendChild(price)
+    document.querySelector('.produto').appendChild(ele)
+})
