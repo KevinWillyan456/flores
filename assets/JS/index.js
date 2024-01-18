6abd18a208452d02788ceb9ea826543c568876e5
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-18 17:00:54 -0400

    style: melhoria geral nos estilos

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,13 +1,11 @@
 const body = document.querySelector('body')
 
-function compra(){
-    var ele = document.createElement('div');
-    ele.classList.add('buy');
+function compra() {
+    var ele = document.createElement('div')
+    ele.classList.add('buy')
     ele.innerHTML = '<p>Parabéns pela compra!</p>'
     ele.addEventListener('animationend', (e) => {
-        setTimeout(
-            () => e.target.remove()
-        ,2000)
+        setTimeout(() => e.target.remove(), 2000)
     })
     body.appendChild(ele)
 }
